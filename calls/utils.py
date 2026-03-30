f69053d262e04ff03779ed43fe16462d291ad0b0
rahul <simpleintelligence.com>
2026-03-30 13:46:03 +0300

    feat: add utility functions to process and import call recordings from filesystem into database

--- calls/utils.py
+++ calls/utils.py
@@ -35,7 +35,7 @@ def process_call_file(wav_path, stdout=None, style=None):
         txt_path = os.path.join(dir_path, txt_filename)
 
         # Check for conversation file in session subdirectory (dialplan: CALL_SESSION_DIR = call_sessions/{CALLER}/{SESSION})
-        conversation_file_path = os.path.join(dir_path, session_id, 'full_conversation.wav')
+        conversation_file_path = os.path.join(dir_path, session_id, 'full_conversation_unfiltered.wav')
 
         full_conv_relative = None
         if os.path.exists(conversation_file_path):
